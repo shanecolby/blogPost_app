89b3863efc7da174a5d1c7d62d1f4d64f693150b
shanecolby <shanecolbyzero8@gmail.com>
2021-10-24 14:07:34 -0500

    loop through the first 5 blog posts and display their title and body content on document

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then(response => response.json())
   .then(data => {
-    const postArr = data.slice(0, 5)
-    console.log(postArr)
+    const postsArr = data.slice(0, 5)
+    let html = ""
+    for (let post of postsArr) {
+      html += `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        <hr />
+      `
+    }
+    document.getElementById("blog-list").innerHTML = html
 
   })
