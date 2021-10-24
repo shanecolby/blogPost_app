d7d4d008b80353bd8b9324ddd8e1fc919b820e0b
shanecolby <shanecolbyzero8@gmail.com>
2021-10-24 14:23:13 -0500

    set up form submit event listener

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     document.getElementById("blog-list").innerHTML = html
 
   })
+
+document.getElementById("new-post").addEventListener("submit", function (e) {
+  e.preventDefault()
+  // stops from reloading page when submitting
+  const postTitle = document.getElementById("post-title").value
+  const postBody = document.getElementById("post-body").value
+  const data = {
+    title: postTitle,
+    body: postBody
+  }
+  console.log(data)
+})
